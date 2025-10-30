ddc0d55ca3c64ea8c42bc45ceff5eb0c6e81858e
Azizah A. Azmi <azizah.aurellia.ft23@mail.umy.ac.id>
2025-10-30 08:49:45 +0700

    feat: add new composable + new function FormDataDiri

--- app/src/main/java/com/example/inputpengguna_prak5_234/TextEdit.kt
+++ app/src/main/java/com/example/inputpengguna_prak5_234/TextEdit.kt
@@ -1,2 +1,9 @@
 package com.example.inputpengguna_prak5_234
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun FormDataDiri(modifier: Modifier){
+
+}
