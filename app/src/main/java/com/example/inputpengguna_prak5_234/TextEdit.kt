1971650d7894f92bd33c2b437106fd6f33928c78
Azizah A. Azmi <azizah.aurellia.ft23@mail.umy.ac.id>
2025-10-30 08:52:36 +0700

    feat: add new variables to remember values from keyboard

--- app/src/main/java/com/example/inputpengguna_prak5_234/TextEdit.kt
+++ app/src/main/java/com/example/inputpengguna_prak5_234/TextEdit.kt
@@ -1,9 +1,15 @@
 package com.example.inputpengguna_prak5_234
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 
 @Composable
 fun FormDataDiri(modifier: Modifier){
-
+    //variabel2 utk mengingat nilai masukan dari keyboard
+    var textNama by remember { mutableStateOf(value = "") }
+    var textAlamat by remember {mutableStateOf(value = "")}
+    var textJK by remember {mutableStateOf(value = "")}
 }
