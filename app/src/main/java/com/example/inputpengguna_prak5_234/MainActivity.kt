a0f019076b438ccc8e20db62c502128ea41f4829
Azizah A. Azmi <azizah.aurellia.ft23@mail.umy.ac.id>
2025-10-30 14:31:37 +0700

    feat: memanggil function FormDataDiri di MainActivity.kt

--- app/src/main/java/com/example/inputpengguna_prak5_234/MainActivity.kt
+++ app/src/main/java/com/example/inputpengguna_prak5_234/MainActivity.kt
@@ -20,28 +20,9 @@ class MainActivity : ComponentActivity() {
         setContent {
             InputPengguna_prak5_234Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    FormDataDiri(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    InputPengguna_prak5_234Theme {
-        Greeting("Android")
     }
 }
